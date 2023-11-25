f039937dc80a33ebc17884eb2888082ae79b984c
yk2099 <149128930+yk2099@users.noreply.github.com>
2023-11-25 00:17:04 -0500

    Add loading indication for map and layer rendering

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -2,25 +2,56 @@ import { Text, Flex, Heading, Box } from "@chakra-ui/react";
 import { SelectComponent } from "../Select";
 
 interface Props {
-  city: string
-  handleCityChange: (pCity: string) =>  void
-  layer: string
-  handleLayerChange: (pLayer: string) => void
-  data: string
-  setData: React.Dispatch<React.SetStateAction<string>>
-  layers: any
+  city: string;
+  handleCityChange: (pCity: string) => void;
+  layer: string;
+  handleLayerChange: (pLayer: string) => void;
+  data: string;
+  setData: React.Dispatch<React.SetStateAction<string>>;
+  layers: any;
 }
 
-export function Navbar({ city, handleCityChange, layer, handleLayerChange, data, setData, layers}: Props ) {
+export function Navbar({
+  city,
+  handleCityChange,
+  layer,
+  handleLayerChange,
+  data,
+  setData,
+  layers,
+}: Props) {
   return (
-    <Flex color={'white'} w={'100%'} p={'0.5rem'} justifyContent={'space-between'} position={'absolute'} top={0} zIndex={1}>
+    <Flex
+      color={"white"}
+      w={"100%"}
+      p={"0.5rem"}
+      justifyContent={"space-between"}
+      position={"absolute"}
+      top={0}
+      zIndex={1}
+    >
       <Heading as="h1">WebGL Traffic Visualization</Heading>
-      <Flex alignItems={'center'} gap={'0.5rem'}>
-        <Text >Select a Dataset: </Text>
-        <SelectComponent options={Object.keys(layers)} value={city} onChange={handleCityChange} placeholder="City"/>
-        <SelectComponent options={city ? Object.keys(layers[city]) : []} value={layer} onChange={handleLayerChange} placeholder="Layer" />
-        <SelectComponent options={layer ? Object.keys(layers[city][layer]) : []} value={data} onChange={setData} placeholder="Data" />
+      <Flex alignItems={"center"} gap={"0.5rem"}>
+        <Text>Select a Dataset: </Text>
+        <SelectComponent
+          options={Object.keys(layers())}
+          value={city}
+          onChange={handleCityChange}
+          placeholder="City"
+        />
+        <SelectComponent
+          options={city ? Object.keys(layers()[city]) : []}
+          value={layer}
+          onChange={handleLayerChange}
+          placeholder="Layer"
+        />
+        <SelectComponent
+          options={layer ? Object.keys(layers()[city][layer]) : []}
+          value={data}
+          onChange={setData}
+          placeholder="Data"
+        />
       </Flex>
     </Flex>
-  )
+  );
 }
--- app/utils/layers.tsx
+++ app/utils/layers.tsx
@@ -2,7 +2,10 @@ import { ScatterplotLayer } from "@deck.gl/layers/typed";
 import { HexagonLayer } from "@deck.gl/aggregation-layers/typed";
 
 /* NY */
-function getScatterPlotNYC(link: string): ScatterplotLayer {
+function getScatterPlotNYC(
+  link: string,
+  onDataLoad: () => void
+): ScatterplotLayer {
   return new ScatterplotLayer({
     id: "scatterplot-layer",
     data: link,
@@ -18,10 +21,14 @@ function getScatterPlotNYC(link: string): ScatterplotLayer {
     lineWidthMinPixels: 1,
     getFillColor: (d) => [255, 140, 255],
     getLineColor: (d) => [124, 252, 0],
+    onDataLoad,
   });
 }
 
-function getHexagonNYC(link: string): HexagonLayer<any> {
+function getHexagonNYC(
+  link: string,
+  onDataLoad: () => void
+): HexagonLayer<any> {
   return new HexagonLayer({
     id: "hexagon-layer",
     data: link,
@@ -30,12 +37,16 @@ function getHexagonNYC(link: string): HexagonLayer<any> {
     radius: 200,
     elevationScale: 4,
     getPosition: (d) => [parseFloat(d.longitude), parseFloat(d.latitude)],
+    onDataLoad,
   });
 }
 /* */
 
 /* LA */
-function getScatterPlotLA(link: string): ScatterplotLayer {
+function getScatterPlotLA(
+  link: string,
+  onDataLoad: () => void
+): ScatterplotLayer {
   return new ScatterplotLayer({
     id: "scatterplot-layer",
     data: link,
@@ -54,10 +65,11 @@ function getScatterPlotLA(link: string): ScatterplotLayer {
     lineWidthMinPixels: 1,
     getFillColor: (d) => [255, 140, 255],
     getLineColor: (d) => [124, 252, 0],
+    onDataLoad,
   });
 }
 
-function getHexagonLA(link: string): HexagonLayer<any> {
+function getHexagonLA(link: string, onDataLoad: () => void): HexagonLayer<any> {
   return new HexagonLayer({
     id: "hexagon-layer",
     data: link,
@@ -69,39 +81,48 @@ function getHexagonLA(link: string): HexagonLayer<any> {
       parseFloat(d.location_1.longitude),
       parseFloat(d.location_1.latitude),
     ],
+    onDataLoad,
   });
 }
 /* */
 
-export const layers: any = {
-  NYC: {
-    "Scatter Plot": {
-      Tree: getScatterPlotNYC(
-        "https://data.cityofnewyork.us/resource/uvpi-gqnh.json"
-      ),
-      Collisions: getScatterPlotNYC(
-        "https://data.cityofnewyork.us/resource/h9gi-nx95.json"
-      ),
+export const layers: any = (onDataLoad: () => void) => {
+  return {
+    NYC: {
+      "Scatter Plot": {
+        Tree: getScatterPlotNYC(
+          "https://data.cityofnewyork.us/resource/uvpi-gqnh.json",
+          onDataLoad
+        ),
+        Collisions: getScatterPlotNYC(
+          "https://data.cityofnewyork.us/resource/h9gi-nx95.json",
+          onDataLoad
+        ),
+      },
+      Hexagon: {
+        Tree: getHexagonNYC(
+          "https://data.cityofnewyork.us/resource/uvpi-gqnh.json",
+          onDataLoad
+        ),
+        Collisions: getHexagonNYC(
+          "https://data.cityofnewyork.us/resource/h9gi-nx95.json",
+          onDataLoad
+        ),
+      },
     },
-    Hexagon: {
-      Tree: getHexagonNYC(
-        "https://data.cityofnewyork.us/resource/uvpi-gqnh.json"
-      ),
-      Collisions: getHexagonNYC(
-        "https://data.cityofnewyork.us/resource/h9gi-nx95.json"
-      ),
+    LA: {
+      "Scatter Plot": {
+        Businesses: getScatterPlotLA(
+          "https://data.lacity.org/resource/6rrh-rzua.json?$limit=100000&$WHERE=location_1 IS NOT NULL",
+          onDataLoad
+        ),
+      },
+      Hexagon: {
+        Businesses: getHexagonLA(
+          "https://data.lacity.org/resource/6rrh-rzua.json?$limit=100000&$WHERE=location_1 IS NOT NULL",
+          onDataLoad
+        ),
+      },
     },
-  },
-  LA: {
-    "Scatter Plot": {
-      Businesses: getScatterPlotLA(
-        "https://data.lacity.org/resource/6rrh-rzua.json?$limit=100000&$WHERE=location_1 IS NOT NULL"
-      ),
-    },
-    Hexagon: {
-      Businesses: getHexagonLA(
-        "https://data.lacity.org/resource/6rrh-rzua.json?$limit=100000&$WHERE=location_1 IS NOT NULL"
-      ),
-    },
-  },
+  };
 };
