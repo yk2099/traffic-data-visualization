1c283a262ad5b8c5e404bc01ce8f94731d1d8f58
yk2099 <149128930+yk2099@users.noreply.github.com>
2023-11-26 21:53:20 -0500

    Add black text color to select options

--- app/components/Select/Select.tsx
+++ app/components/Select/Select.tsx
@@ -1,19 +1,31 @@
-"use client"
+"use client";
 
-import { Select } from "@chakra-ui/react";
+import { Select, Text } from "@chakra-ui/react";
 
 interface Props {
-  options: string[]
-  value: string
-  onChange: (param: string) => void
-  placeholder: string
+  options: string[];
+  value: string;
+  onChange: (param: string) => void;
+  placeholder: string;
 }
 
-export function SelectComponent({ options, value, onChange, placeholder }: Props) {
+export function SelectComponent({
+  options,
+  value,
+  onChange,
+  placeholder,
+}: Props) {
   return (
-    <Select w={'fit-content'} value={value} onChange={e => onChange(e.target.value)} placeholder={placeholder}>
+    <Select
+      w={"fit-content"}
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      placeholder={placeholder}
+    >
       {options.map((option) => (
-        <option key={option} value={option}>{option}</option>
+        <option key={option} value={option}>
+          <Text color={'black'}>{option}</Text>
+        </option>
       ))}
     </Select>
   );
